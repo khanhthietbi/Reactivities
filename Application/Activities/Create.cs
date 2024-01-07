f6929e2397ac5441449aebf7bef84b1e532dcd6d
khanhthietbi <khanthietbi@gmail.com>
2024-01-07 18:32:16 +0700

    End of section 4

---
+++ Application/Activities/Create.cs
@@ -0,0 +1,37 @@
+
+using Domain;
+using MediatR;
+using Persistence;
+using static Application.Activities.Create;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+        private readonly DataContext _context;
+
+        public Handler()
+        {
+        }
+
+
+        public Handler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(Command request, CancellationToken cancellationToken)
+        {
+            _context.Activities.Add(request.Activity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
